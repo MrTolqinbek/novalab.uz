f801a5405e2c6be068f03dcd20d9400155a06ef8
Tolqinbek Ermatov <jamoliddinovichtolqin@gmail.com>
2021-08-14 03:43:18 -0400

    last

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
+const metric = document.querySelector(".metrics-box");
+let avail = true;
 window.addEventListener("scroll", (e) => {
+  if (avail) {
+    if (window.pageYOffset + window.innerHeight > metric.offsetTop) {
+      avail = false;
+      Array(...metric.children).forEach((e) => {
+        let a = 0;
+        let inet = setInterval(() => {
+          if (a >= 100) {
+            clearInterval(inet);
+          } else {
+            a+=5;
+            e.children[1].innerHTML = a;
+          }
+        }, 100);
+      });
+    }
+  }
+
   if (window.pageYOffset > 300) {
     document.querySelector("nav").className = "fixed";
     document.querySelector(".inner").className = "inner container";
-    document.querySelector('.move-top-x').className = "move-top-x move-top"  
-} else {
+    document.querySelector(".move-top-x").className = "move-top-x move-top";
+  } else {
     document.querySelector(".inner").className = "inner";
     document.querySelector("nav").className = "";
-    document.querySelector('.move-top-x').className = "move-top-x"  
+    document.querySelector(".move-top-x").className = "move-top-x";
   }
 });
 document.querySelector(".burger").addEventListener("click", () => {
@@ -21,24 +40,34 @@ document.querySelectorAll(".transition").forEach((el) => {
     let X = -a.left + e.clientX;
     let width = a.width;
     let height = a.height;
-    el.style.transform = ` perspective(1010px) rotateX(${(width / 2 - X) / 10}deg) rotateY(${
-      (height / 2 - Y) / 10
-    }deg)`;
+    el.style.transform = ` perspective(1010px) rotateX(${
+      (width / 2 - X) / 10
+    }deg) rotateY(${(height / 2 - Y) / 10}deg)`;
   });
 
-  el.addEventListener('mouseleave',()=>{
+  el.addEventListener("mouseleave", () => {
     el.style.transform = ` perspective(1010px) rotateX(0deg) rotateY(0deg)`;
-})
+  });
 });
 
-document.querySelector('.move-top-x').addEventListener('click',()=>{
- 
-    let interval = setInterval(()=>{
+document.querySelector(".move-top-x").addEventListener("click", () => {
+  let interval = setInterval(() => {
+    if (window.pageYOffset < 10) {
+      clearInterval(interval);
+    }
+    window.scrollTo(0, window.pageYOffset - 20);
+  }, 10);
+});
 
-if(window.pageYOffset<10){
-    clearInterval(interval)
+let ca = document.querySelector("form").children;
+for (let i = 0; i < ca.length; i++) {
+  ca[i].children[0].addEventListener("input", () => {
+    if (i != 4) {
+      if (ca[i].children[0].value) {
+        ca[i].children[1].className = "input-in";
+      } else {
+        ca[i].children[1].className = "";
+      }
+    }
+  });
 }
-        window.scrollTo(0,window.pageYOffset-20)
-    },10)
-
-})  
